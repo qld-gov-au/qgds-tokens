5c8388233b9e9a782dd8c3599f10f909baf5140a
Pedro Portella <contact@pedroportella.com>
2024-08-27 08:27:23 +1000

    feat: fg, bg and accent extensions

--- src/js/button/button-qld-maroon-light.tokens.js
+++ src/js/button/button-qld-maroon-light.tokens.js
@@ -2,7 +2,7 @@
  * Do not edit directly, this file was auto-generated.
  */
 
-export const buttonPrimaryBackground = "#09549f";
+export const buttonPrimaryBackground = "#0a549f";
 export const buttonPrimaryText = "#ffffff";
 export const buttonSecondaryBorderColor = "#77ba02";
 export const buttonSecondaryText = "#121940";
