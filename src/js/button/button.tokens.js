4fa99798bccf7e7eb79bd4169356e1c70955314d
Pedro Portella <contact@pedroportella.com>
2024-09-19 15:42:40 +1000

    feat: type-scale

--- src/js/button/button.tokens.js
+++
@@ -1,7 +0,0 @@
-/**
- * Do not edit directly, this file was auto-generated.
- */
-
-export const buttonBgSecondaryBackground = "accent.bg.action.button.secondary.background}";
-export const buttonBorderRadius = "8";
-export const buttonBorderWidth = "8px";
