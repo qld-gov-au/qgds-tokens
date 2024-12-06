f819153b1f35b37a3c7a08314da27a54cafdcf24
Pedro Portella <contact@pedroportella.com>
2024-12-06 13:13:34 +1000

    feat: build 004

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+// Please edit original file located in .build/original/index.ts
+export default qgdsDesignTokens;
+import cssTokensPrimitive from './css/styles/primitive.css';
+function qgdsDesignTokens() {
+    return JSON.stringify(cssTokensPrimitive);
+}
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+// Please edit original file located in .build/original/index.ts
+
+export default qgdsDesignTokens;
+import cssTokensPrimitive from './css/styles/primitive.css';
+
+function qgdsDesignTokens() {
+  return JSON.stringify(cssTokensPrimitive);
+}
