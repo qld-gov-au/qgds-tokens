bd6fa336a75b47ea55d433c2120feb1e3b5e5b87
Pedro Portella <contact@pedroportella.com>
2024-12-18 14:41:36 +1000

    feat: border-radius in px

--- src/js/button/button.js
+++ src/js/button/button.js
@@ -2,5 +2,5 @@
  * Do not edit directly, this file was auto-generated.
  */
 
-export const buttonBorderRadius = "32";
+export const buttonBorderRadius = "32px";
 export const buttonBorderWidth = "8px";
