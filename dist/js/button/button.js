69b7702a46ee284fb5f9abbc569e79ecb188c569
Pedro Portella <contact@pedroportella.com>
2025-01-31 10:40:40 +1000

    feat: recycle new colours

---
+++ dist/js/button/button.js
@@ -0,0 +1,6 @@
+/**
+ * Do not edit directly, this file was auto-generated.
+ */
+
+export const buttonBorderRadius = "32px";
+export const buttonBorderWidth = "8px";
---
+++ dist/js/card/card.js
@@ -0,0 +1,6 @@
+/**
+ * Do not edit directly, this file was auto-generated.
+ */
+
+export const cardBorderRadius = "12px";
+export const cardPadding = "16px";
