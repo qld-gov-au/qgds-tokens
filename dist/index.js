d0267d8822f138a990b6642d1c6a75b0a5bd51ff
Steven Lynagh <steve.lynagh@smartservice.qld.gov.au>
2025-12-16 09:52:44 +1000

    remove cached dist files

--- dist/index.js
+++
@@ -1,6 +0,0 @@
-// Please edit original file located in .build/original/index.ts
-export default qgdsDesignTokens;
-import cssTokensPrimitive from './css/styles/primitive.css';
-function qgdsDesignTokens() {
-    return JSON.stringify(cssTokensPrimitive);
-}
